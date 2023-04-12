d273481b9f35a92c01a165c5b04fc3bb11dc5334
SaToGo <stoledano97@gmail.com>
2023-04-12 22:05:27 +0200

    Modified Footer so it shows copyright.
    shows the copyright of the icons that are not yet added to the page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ const Footer = () => {
 
     return (
         <footer style={footerStyle()}>
-            <a style={whiteStyle()} href="https://github.com/SaToGo1"><p>SaToGo1</p></a>
+            <a style={whiteStyle()} href="https://github.com/SaToGo1"><p style={noMarginStyle()}>SaToGo1</p></a>
+            <a style={whiteStyle()} href="https://boxicons.com/"><p style={noMarginStyle()}>Icons by Copyright (c) 2015-2021 Aniket Suvarna</p></a>
         </footer>
     )
 }
@@ -13,15 +14,22 @@ export default Footer;
 const footerStyle = () => {
     return {
         display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
         color: "white",
-        marginRight: "auto",
         marginLeft: "20px",
-        height: "20px",
+        minHeight: "20px",
     }
 }
 
 const whiteStyle = () => {
     return {
         color: "white",
+    }
+}
+
+const noMarginStyle = () => {
+    return {
+        margin: "0",
     }
 }
