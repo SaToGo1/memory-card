4f3d62356d4de4ba1e23ce5ad4d14456d48f85fc
SaToGo <stoledano97@gmail.com>
2023-04-12 22:06:23 +0200

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+
+const Card = () => {
+
+    return (
+        <div>
+            <div>
+                <img />
+            </div>
+            <div>
+                <p>This is a card</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
